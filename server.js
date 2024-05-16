b1bf0e0156c9ac075661ebc86b8b7c13ab9c6fea
maulana saputra <maulana.saputra@aqi.co.id>
2024-05-16 17:11:28 +0700

    percobaan 4
    Signed-off-by: maulana saputra <maulana.saputra@aqi.co.id>

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const options = {
 app.use(cors(options));
 
 app.get("/", (req, res) => {
-  res.status(200).send("selamat datang di web saya percobaan 3");
+  res.status(200).send("selamat datang di web saya percobaan 4");
 });
 
 const PORT = process.env.PORT || 5000;
